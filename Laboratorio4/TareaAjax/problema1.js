a78b4c7c0f8d2892e2aaeae40db64d103d25b49f
Calihn1 <ehilacondob@unsa.edu.pe>
2025-05-03 20:35:33 -0500

    Al mandar el arreglo a la funcion, busca las regiones unicas, las ordena alfabeticamente y despues crea una lista y despues la reemplazamos en el html

---
+++ Laboratorio4/TareaAjax/problema1.js
@@ -0,0 +1,27 @@
+
+document.addEventListener('DOMContentLoaded', () => {
+    fetch('data.json')
+      .then(res => res.json())
+      .then(data => listRegions(data))
+      .catch(err => console.error('Error cargando data.json:', err));
+  });
+  
+ 
+  function listRegions(data) {
+    const regiones = data.map(item => item.region);
+  
+    const únicas = [...new Set(regiones)];
+  
+    únicas.sort();
+  
+    const ul = document.createElement('ul');
+    únicas.forEach(r => {
+      const li = document.createElement('li');
+      li.textContent = r;
+      ul.appendChild(li);
+    });
+  
+    const cont = document.getElementById('list-regions');
+    cont.appendChild(ul);
+  }
+  
