eec7ac7691a83cce0c6b2ba02b6c2b488d9d6301
Calihn1 <ehilacondob@unsa.edu.pe>
2025-05-03 20:49:51 -0500

    como es un objeto, lo volvemos en un numero y los enumeramos y se va sumando de esa manera sabemos cuantos caso tiene cada región, generamos la una tabla y despues la mandamos al DOM

--- Laboratorio4/TareaAjax/problema2.js
+++ Laboratorio4/TareaAjax/problema2.js
@@ -1,6 +1,35 @@
+// 2.js
 document.addEventListener('DOMContentLoaded', () => {
     fetch('data.json')
       .then(res => res.json())
       .then(data => showTotals(data))
       .catch(err => console.error('Error cargando data.json:', err));
   });
+  
+  function showTotals(data) {
+    const totales = {};
+  
+    data.forEach(entry => {
+      const region = entry.region;
+  
+    
+      const sumaConfirmados = entry.confirmed
+        .map(obj => Number(obj.value))     
+        .reduce((acc, curr) => acc + curr, 0); 
+
+      totales[region] = sumaConfirmados;
+    });
+  
+   
+    let html = '<table>';
+    html += '<tr><th>Región</th><th>Total Confirmados</th></tr>';
+    for (const region in totales) {
+      html += `<tr>
+                 <td>${region}</td>
+                 <td>${totales[region]}</td>
+               </tr>`;
+    }
+    html += '</table>';
+  
+    document.getElementById('totals-region').innerHTML = html;
+  }
