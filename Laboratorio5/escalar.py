0fa4cc8226621513c14519d2064f32d8f5d03f26
Calihn1 <ehilacondob@unsa.edu.pe>
2025-05-10 09:35:32 -0500

    De la función ya establecido para verificar si es un escalar, se creó una matriz para pobrarlo

---
+++ Laboratorio5/escalar.py
@@ -0,0 +1,20 @@
+def esEscalar(m):
+    d = m[0][0] 
+    for i in range(len(m)):
+        for j in range(len(m)):
+            if i != j:
+                if m[i][j] != 0:
+                    print(m[i][j])
+                    return False
+            elif m[i][j] != d:
+                print(m[i][j])
+                return False
+    return True
+
+m = [
+    [2, 0, 0],
+    [0, 2, 0],
+    [0, 0, 2]
+]
+
+print(esUnitaria(m))  
