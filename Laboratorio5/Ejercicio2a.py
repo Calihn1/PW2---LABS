7dc95980292e5d93ca5a88bfe7b5abd33170cd19
Calihn1 <ehilacondob@unsa.edu.pe>
2025-05-10 18:45:49 -0500

    Se dibujo dos caballos negros y dos blancos, dos arriba y dos abajo

---
+++ Laboratorio5/Ejercicio2a.py
@@ -0,0 +1,9 @@
+from interpreter import draw
+from chessPictures import *
+from picture import *
+
+
+caballos1 = knight.negative().join(knight)
+caballos2 = knight.join(knight.negative())
+caballos3 = caballos1.up(caballos2)
+draw(caballos3)
