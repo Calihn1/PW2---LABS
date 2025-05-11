569ab3b90765fedae997fec5f963f15f1108538d
Calihn1 <ehilacondob@unsa.edu.pe>
2025-05-10 20:28:43 -0500

    Se dibujó a todos los peones

--- Laboratorio5/Ejercicio2g.py
+++ Laboratorio5/Ejercicio2g.py
@@ -6,29 +6,40 @@ def generarTablero():
     tab = square.negative()
     tab = tab.join(square)
     tab = tab.join(tab)
+                   
     tab = tab.join(tab)
 
     tab = tab.up(tab.verticalMirror())
-    tab = tab.up(tab)
+    
     return tab
 
-def generarPiezas(isNegative):
+def generarPiezas():
     piezas = [knight, bishop, queen, king, bishop, knight, rock]
+    
+    tab = square.negative()
+    tab = tab.under(rock)
 
-    if(not isNegative):
-        tab = square.negative()
-        tab = tab.under(rock)
+    for i in range(0,7):
+        if i % 2 == 0:
+            tab = tab.join(square.under(piezas[i]))
+        else:
+            casilla = square.negative()
+            tab = tab.join(casilla.under(piezas[i]))
+        
+    peon = square
+    peon = peon.under(pawn)
+    peon2 = square.negative()
+    peon2 = peon2.under(pawn)
+    peones = peon.join(peon2)
+    peones = peones.join(peones)
+    peones = peones.join(peones)
 
-        for i in range(0,7):
-            if i % 2 == 0:
-                tab = tab.join(square.under(piezas[i]))
-            else:
-                casilla = square.negative()
-                tab = tab.join(casilla.under(piezas[i]))
+    tab = tab.up(peones)
 
-        return tab 
+    return tab 
                
 
 
-tablero = generarPiezas(False)
+tablero = generarPiezas()
+
 draw(tablero)
