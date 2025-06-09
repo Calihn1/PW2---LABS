f0fe9e5c6ef951f9784680472d0d7c15ebcaf70b
Calihn1 <ehilacondob@unsa.edu.pe>
2025-06-08 22:35:06 -0500

    Sexto commit: Se creó un view y un html donde se podra descargar, ademas se actualizo las url

--- Laboratorio8/models_examples/models_examples/urls.py
+++ Laboratorio8/models_examples/models_examples/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from pdf_generator import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('send.urls')),
+    path('generate-pdf/', views.pdf_view, name='generate_pdf'),
 ]
--- Laboratorio8/models_examples/pdf_generator/views.py
+++ Laboratorio8/models_examples/pdf_generator/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+# views.py
 
-# Create your views here.
+import datetime
+from django.http import Http404
+from models_examples import renderers
+
+def pdf_view(request):
+    data = {
+        'today': datetime.date.today(),
+        'amount': 39.99,
+        'customer_name': 'Cooper Mann',
+        'invoice_number': 1233434,
+        'date': datetime.date.today().strftime('%Y-%m-%d'),
+        'pdf_title': 'Invoice #1233434',
+    }
+    response = renderers.render_to_pdf('pdf_generator/invoice.html', data)
+    if response.status_code == 404:
+        raise Http404("Invoice not found")
+
+    filename = f"Invoice_{data['invoice_number']}.pdf"
+    content = f'inline; filename={filename}'
+    if request.GET.get("download"):
+        content = f'attachment; filename={filename}'
+    response["Content-Disposition"] = content
+    return response
