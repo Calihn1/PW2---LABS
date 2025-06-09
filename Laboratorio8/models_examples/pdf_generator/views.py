ace27f4f58a218f5c7d67c90b6fed86dbb2cb810
Calihn1 <ehilacondob@unsa.edu.pe>
2025-06-08 22:51:49 -0500

    Noveno commit: Se actualizo views de acuerdo al enfoque de xhtml2pdf

--- Laboratorio8/models_examples/pdf_generator/views.py
+++ Laboratorio8/models_examples/pdf_generator/views.py
@@ -1,25 +1,23 @@
-# views.py
-
 import datetime
 from django.http import Http404
-from models_examples import renderers
+from models_examples.renderers import render_to_pdf
 
 def pdf_view(request):
     data = {
-        'today': datetime.date.today(),
-        'amount': 39.99,
+        'today':   datetime.date.today(),
+        'amount':  39.99,
         'customer_name': 'Cooper Mann',
         'invoice_number': 1233434,
-        'date': datetime.date.today().strftime('%Y-%m-%d'),
-        'pdf_title': 'Invoice #1233434',
+        'date':    datetime.date.today().strftime('%Y-%m-%d'),
+        'pdf_title':     'Invoice #1233434',
     }
-    response = renderers.render_to_pdf('pdf_generator/invoice.html', data)
-    if response.status_code == 404:
+    response = render_to_pdf('pdfs/invoice.html', data)
+    if response.status_code == 400:
         raise Http404("Invoice not found")
 
     filename = f"Invoice_{data['invoice_number']}.pdf"
-    content = f'inline; filename={filename}'
-    if request.GET.get("download"):
-        content = f'attachment; filename={filename}'
-    response["Content-Disposition"] = content
+    disposition = 'inline; filename=' + filename
+    if request.GET.get('download'):
+        disposition = 'attachment; filename=' + filename
+    response['Content-Disposition'] = disposition
     return response
