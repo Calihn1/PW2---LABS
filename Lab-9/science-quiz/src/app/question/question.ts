f4091349dd0b4fdfa77c22d60686c5356c1862e9
Calihn1 <ehilacondob@unsa.edu.pe>
2025-06-29 20:58:23 -0500

    Decimo commit: Se agrego la logica de question, mostrar las opciones, el feedback o si termino el quiz

--- Lab-9/science-quiz/src/app/question/question.ts
+++ Lab-9/science-quiz/src/app/question/question.ts
@@ -1,11 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ActivatedRoute, RouterModule, Router } from '@angular/router';
+import { QuestionService } from '../question-service';
 
 @Component({
+  standalone: true,
   selector: 'app-question',
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './question.html',
-  styleUrl: './question.css'
+  styleUrls: ['./question.css']
 })
 export class Question {
+  private route = inject(ActivatedRoute);
+  private router = inject(Router);
+  private questionService = inject(QuestionService);
 
+  questionIndex = 0;
+  question: any;
+  feedback = '';
+
+  constructor() {
+    this.route.paramMap.subscribe((params) => {
+      this.questionIndex = Number(params.get('id'));
+      this.question = this.questionService.getQuestions()[this.questionIndex];
+      this.feedback = ''; 
+    });
+  }
+
+  select(option: string) {
+    this.feedback = option === this.question.correctAnswer ? '✅ Correcto' : '❌ Incorrecto';
+  }
+
+  next() {
+    const nextIndex = this.questionIndex + 1;
+    if (nextIndex < this.questionService.getQuestions().length) {
+      this.router.navigate(['/question', nextIndex]);
+    } else {
+      this.router.navigate(['/results']);
+    }
+  }
 }
