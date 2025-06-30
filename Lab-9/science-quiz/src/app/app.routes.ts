c0d3d42ed483231cbc4ee76d5e6725850c071ce8
Calihn1 <ehilacondob@unsa.edu.pe>
2025-06-29 20:32:31 -0500

    Cuarto commit: Se agrego las rutas para cada componente

--- Lab-9/science-quiz/src/app/app.routes.ts
+++ Lab-9/science-quiz/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home';
+import { Question } from './question/question';
+import { ResultsComponent } from './results/results';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'question/:id', component: Question },
+  { path: 'results', component: ResultsComponent },
+];
